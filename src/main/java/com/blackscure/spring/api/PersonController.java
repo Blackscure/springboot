fbb4bc774aae7e4c709f0ac0470ec920d16018b3
blackscure <Wekesabuyahi@gmail.com>
2020-08-03 12:59:13 +0300

    resquestmapping and restcontroller aaded in personController file

--- src/main/java/com/blackscure/spring/api/PersonController.java
+++ src/main/java/com/blackscure/spring/api/PersonController.java
@@ -2,14 +2,22 @@ package com.blackscure.spring.api;
 
 import com.blackscure.spring.model.Person;
 import com.blackscure.spring.service.PersonService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("api/v1/person")
+@RestController
 public class PersonController {
     private final PersonService personService;
 
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
-    public void addPerson(Person person) {
+
+    @PostMapping
+    public void addPerson(@RequestBody Person person) {
         personService.addPerson(person);
     }
 }
